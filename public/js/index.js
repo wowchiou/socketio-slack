2a1ca12635a393a8372b312db8290b3bb50bcd22
Aaren Chiou <wowchiou@gmail.com>
2020-02-05 17:54:12 +0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,98 @@
 (function() {
-  //   const username = prompt('你的名字');
-  const socket = io('http://localhost:9000');
+  const username = prompt('你的名字');
+  let nsSocket = '';
 
+  // 連結 socket.io server
+  const socket = io('http://localhost:9000', {
+    query: {
+      username
+    }
+  });
+
+  // socket 監聽 server 發送的 nsList 事件
   socket.on('nsList', list => {
-    let namespaceDiv = '';
+    // 收到 namespace 資料後,渲染 namespace 畫面
+    buildNamespaceHtml(list);
 
-    list.forEach(ns => {
+    // 預設進入的 nsSocket
+    joinNs('/mouse');
+  });
+
+  function joinNs(endpoint) {
+    // 監測 nsSocket 是否有連接
+    // 有連接中的 nsSocket 關閉連接
+    // 並取消使用者聊天框的 submit 事件
+    if (nsSocket) {
+      nsSocket.close();
+      // $('#user-input').unbind('submit', formSubmission);
+    }
+
+    // 連結新的 nsSocket
+    nsSocket = io(`http://localhost:9000${endpoint}`);
+
+    // nsSocket 監聽後端傳來的房間資料
+    nsSocket.on('nsRoomLoad', nsRooms => {
+      // 收到房間資料後,渲染房間畫面
+      buildRoomsHtml(nsRooms);
+
+      // 預設進入的 room
+      joinRoom(nsRooms[0].title);
+    });
+  }
+
+  function joinRoom(roomName) {
+    // 進入房間
+    nsSocket.emit('joinRoom', roomName);
+
+    // 從後端得到該房間人數,並更新至畫面
+    nsSocket.on('updateRoomData', roomData => {
+      $('.num-users').text(roomData.numberOfMembers);
+      $('.curr-room-text').text(roomData.roomName);
+    });
+
+    // 從後端得到該房間聊天歷史,並更新畫面
+    nsSocket.on('historyCatchUp', history => {
+      $('#messages').empty();
+      if (history.length !== 0) {
+        history.forEach(obj => {
+          buildMessageHtml(obj);
+        });
+      }
+    });
+
+    // 從後端得到完整 message 資訊,並更新畫面
+    nsSocket.on('msgToClient', msg => {
+      buildMessageHtml(msg);
+    });
+  }
+
+  $(document).on('click', '.namespace', function() {
+    // namespace 點擊後連結對應的 nsSocket
+    const nsEndpoint = getNsEndpoint($(this));
+    joinNs(nsEndpoint);
+  });
+
+  $(document).on('click', '.room', function() {
+    // room 點擊後連結對應的聊天室
+    const room = $(this).text();
+    joinRoom(room);
+  });
+
+  // 發送訊息給 nsSocket
+  $(document).on('submit', '#user-input', function(e) {
+    e.preventDefault();
+    const message = $('#user-message').val();
+    nsSocket.emit('msgToServer', message);
+    $('#user-message').val('');
+  });
+
+  /////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
+
+  function buildNamespaceHtml(data) {
+    let namespaceDiv = '';
+    data.forEach(ns => {
       namespaceDiv += `<div class="namespace" ns=${ns.endpoint} ><span><img src=".${ns.img}" alt="${ns.title}" title="${ns.title}聊天區" /></span></div>`;
     });
 
@@ -17,14 +104,39 @@
       .find('.namespace')
       .eq(0)
       .addClass('active');
+  }
 
-    $(document).on('click', '.namespace', function() {
-      $('.namespace').removeClass('active');
-      $(this).addClass('active');
-      const nsEndpoint = $(this).attr('ns');
-      joinNs(nsEndpoint);
+  function buildRoomsHtml(data) {
+    let roomList = '';
+    data.forEach(room => {
+      let roomPrivate = 'globe';
+      room.roomPrivate && (roomPrivate = 'lock');
+      roomList += `<li class="room"><span class="glyphicon glyphicon-${roomPrivate}"></span>${room.title}</li>`;
     });
+    $('.room-list')
+      .empty()
+      .append(roomList);
+  }
 
-    joinNs('/mouse');
-  });
+  function buildMessageHtml(msg) {
+    const currentTime = new Date(msg.time).toLocaleString();
+    let msgStr = `
+    <li>
+      <div class="user-image">
+        <img src="${msg.avatar}" />
+      </div>
+      <div class="user-message">
+        <div class="user-name-time">${msg.username}<span>${currentTime}</span></div>
+        <div class="message-text">${msg.text}</div>
+      </div>
+    </li>`;
+    $('#messages').append(msgStr);
+    $('#messages').scrollTo(0, $('#messages').scrollHeight);
+  }
+
+  function getNsEndpoint(el) {
+    $('.namespace').removeClass('active');
+    el.addClass('active');
+    return el.attr('ns');
+  }
 })();
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const socketio = require('socket.io');
+
+let currentRoom = '';
+let currentNamespace = '';
+
+let username = '';
+let avatar = `https://pkq.herokuapp.com/static/Icon/${Math.floor(
+  Math.random() * 151
+) + 1}.png`;
+
+// 引入 namespaces 資料
 const namespaces = require('./data/namespaces');
 
+// 建立 express server
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
+// 建立 socket.io server 並聆聽 9000 port
+const PORT = 9000;
 const io = socketio(
-  app.listen(9000, () => {
+  app.listen(PORT, () => {
     console.log('This is socket.io server!');
   })
 );
 
+// socket.io server 做 main socket 連結事件
 io.on('connection', socket => {
   const nsData = namespaces.map(ns => {
     return {
@@ -20,12 +34,81 @@ io.on('connection', socket => {
       endpoint: ns.endpoint
     };
   });
+
+  // 取得 user name
+  username = socket.handshake.query.username;
+
+  // main socket 發送 namespaces 資料給前端
   socket.emit('nsList', nsData);
 });
 
 namespaces.forEach(ns => {
+  // 每個 namespace 做各自的 nsSocket 連結事件
   io.of(ns.endpoint).on('connection', nsSocket => {
-    const username = nsSocket.handshake.query.username;
+    // nsSocket 被連結後發送房間資料給前端
     nsSocket.emit('nsRoomLoad', ns.rooms);
+
+    // 監聽進入新房間
+    nsSocket.on('joinRoom', roomToJoin => {
+      // 離開原先房間,並更新離開的房間人數
+      if (currentRoom) {
+        nsSocket.leave(currentRoom);
+        updateUserInRoom(currentNamespace, currentRoom);
+      }
+
+      // 進入新房間
+      nsSocket.join(roomToJoin);
+
+      // 更新新房間人數
+      updateUserInRoom(ns.endpoint, roomToJoin);
+
+      // 回傳新房間的留言紀錄
+      const roomHistory = getRoomInfo(ns.rooms, roomToJoin).history;
+      nsSocket.emit('historyCatchUp', roomHistory);
+
+      // 紀錄目前 room 與 ns
+      currentRoom = roomToJoin;
+      currentNamespace = ns.endpoint;
+    });
+
+    // 監聽前端發送新訊息
+    nsSocket.on('msgToServer', msg => {
+      // 建立完整 message 資訊
+      const fullInfo = {
+        text: msg,
+        time: Date.now(),
+        avatar,
+        username
+      };
+
+      // 將完整 message 加到該房間的歷史紀錄裡
+      const roomState = getRoomInfo(ns.rooms, currentRoom);
+      roomState.addMessage(fullInfo);
+
+      // 只針對目前房間發送完整 message 資訊給前端
+      io.of(ns.endpoint)
+        .to(currentRoom)
+        .emit('msgToClient', fullInfo);
+    });
   });
 });
+
+function updateUserInRoom(endpoint, room) {
+  io.of(endpoint)
+    .in(room)
+    .clients((err, client) => {
+      io.of(endpoint)
+        .in(room)
+        .emit('updateRoomData', {
+          numberOfMembers: client.length,
+          roomName: room
+        });
+    });
+}
+
+function getRoomInfo(rooms, nowRoom) {
+  const result = rooms.find(room => {
+    return room.title === nowRoom;
+  });
+  return result;
+}
